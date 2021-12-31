586fa51ab49fe466481eaeb336eff619c67d9278
jiaruifang <fangjiarui123@gmail.com>
2021-12-31 16:04:45 +0800

    polish code

--- zhua.py
+++ zhua.py
@@ -1,18 +1,60 @@
-from requests_html import HTMLSession
 from urllib.parse import urlparse
 import requests
-session = HTMLSession()
 from bs4 import BeautifulSoup
 import pandas as pd
 import json
 from pandas import json_normalize
 import pycountry
 import os
+import shutil
+import tempfile
+import urllib
+from urllib.request import urlopen
+import nltk
 
-def ranking_log_to_cities(file_name):
+
+RES_DIR = "tomtom_data"
+
+
+def ranking_log_to_cities():
     r"""
-    ranking log to city and city code
+    ranking log to city code
     """
+    
+    url = 'https://www.tomtom.com/en_gb/traffic-index/ranking/'
+    # html = urlopen(url).read()
+    
+    # print(raw)
+
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    print(r.text)
+
+    raw = soup.find('div', class_="RankingTable__td RankingTable__td--city").text
+    badges = soup.body.find('div', attrs={'class': 'badges'})
+    for span in badges.span.find_all('span', recursive=False):
+        print(span.attrs['RankingTable__td RankingTable__td--city'])
+    print(raw)
+    exit(0)
+    # print(soup.find_all('a'))
+    for script in soup(["script", "style"]):
+        script.extract()
+    text = soup.get_text()
+
+    lines = (line for line in text.splitlines())
+
+    # # break multi-headlines into a line each
+    chunks = (phrase for line in lines for phrase in line.split("  "))
+    # # drop blank lines
+    # for chunk in chunks:
+    #     if chunk:
+    #         print(chunk)
+    text = ''.join(chunk or print('chunk ',chunk) for chunk in chunks if chunk)
+    # print(text)
+    with open("tmpfile.txt", 'w+') as tmp_file:
+        tmp_file.write(text)
+    
+    exit(0)
     with open(file_name, 'r+') as file:
         is_enter = False
         cnt = 0;
@@ -36,12 +78,22 @@ def ranking_log_to_cities(file_name):
         
         city_code_list = []
         for elem in zip(city_name_list, country_name_list):
-            ctry_code = pycountry.countries.search_fuzzy(elem[1])
+            # UAE is not support by the search_fuzzy function
+            try:
+                ctry_code = pycountry.countries.search_fuzzy(elem[1]).alpha_3
+            except:
+                # for UAE
+                ctry_code = elem[1].upper()
             city_code_list.append(f'{ctry_code}_{elem[0]}')
         print(len(city_code_list))
         print(city_code_list)
+        return city_code_list
 
 def get_city_code(file):
+    r"""
+    Yet another way to get the city code
+    alternative to the ranking_log_to_cities
+    """
     df = pd.read_csv(file)
     print(df)
     return df
@@ -58,13 +110,17 @@ def url_to_dataframe(ccode, city):
     return df
 
 if __name__ == "__main__":
-    os.mkdir('res')
+    try:
+        os.stat(RES_DIR)
+    except:
+        os.mkdir(RES_DIR)
+    # ranking_log_to_cities('./config/log')
+    
     city_df = get_city_code("./config/TOMTOM-Country_city_list_rev.csv")
     cities = city_df.to_numpy()
-    print(cities)
     for line in cities:
         ccode = line[1]
         city = line[2]
         print(f"processing {ccode} {city}")
         res_df = url_to_dataframe(ccode, city)
-        res_df.to_csv(f'./res/{ccode}_{city}.csv', index=False)
+        res_df.to_csv(f'./{RES_DIR}/{ccode}_{city}.csv', index=False)
